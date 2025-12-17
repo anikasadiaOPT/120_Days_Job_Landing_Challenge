678f68ed8549763e69055979f88746a98815ac7a
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-17 18:30:29 +0000

    Added solution of the Slicing Surgeon

---
+++ 04_day/Challenge_02.py
@@ -0,0 +1,7 @@
+# The Slicing Surgeon
+data = [10, 20, 30, 40, 50]
+
+updated_data = data[:1:-1]
+
+print(f"The Original Data: {data}")           # The output: [10, 20, 30, 40, 50]
+print(f"The updated data: {updated_data}")    # The output of updated_data: [50, 40, 30]
