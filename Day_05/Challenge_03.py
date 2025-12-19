f006c32684837eb72ccca8f7faff92790c475ee8
Sadia Afrin Anika <141661782+anikasadiaOPT@users.noreply.github.com>
2025-12-19 17:19:23 +0600

    Added the solution of The Frequency Counter

--- Day_05/Challenge_03.py
+++ Day_05/Challenge_03.py
@@ -12,6 +12,7 @@ for ch in word:
     else:
         frequency[ch] = frequency.get(ch, value) + 1    # Incremented by 1 if the character is seen before
 
+
 # The Output
 for key, value in frequency.items():
     print(key, value)
