94985ed077f54da0b4494f77388695e713ee9b50
Sadia Afrin Anika <anikasadia2723@gmail.com>
2026-01-14 20:22:50 +0600

    Added the Anonymous Function's solution.

---
+++ Month_01/Week_02/Day_11/Challenge_01.py
@@ -0,0 +1,13 @@
+# The Anonymous Function
+
+# Standard Definition 
+def std_add(a,b):
+    return a + b
+
+# Lambda Definition 
+lambda_add = lambda a, b: a + b
+
+
+# print
+print(f"From Standard Function: {std_add(5, 10)}")
+print(f"From Lambda Function: {lambda_add(15, 10)}")
