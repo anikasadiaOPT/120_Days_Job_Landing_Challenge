7e8b76b4c156b0f74ae0c38317f2d1692554c37c
Sadia Afrin Anika <anikasadia2723@gmail.com>
2026-01-29 16:03:29 +0600

    Updated the folder

--- Month_01/Week_02/Day_13/Challenge_01.py
+++ Month_01/Week_02/Day_13/Challenge_01.py
@@ -1,9 +1,6 @@
 # The Safe Open
 
+with open("Anika.txt", "w") as f:
+    f.write("Hello World!\n")
+    
 
-with open("Anika.txt", "a") as f:
-    f.write("\n Hello World!")
-
-with open("Anika.txt", "r") as f:
-    file = f.read()
-    print(file)                                  # Output:  Hello World!
