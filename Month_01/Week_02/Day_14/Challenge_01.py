c779b969801e0330fbc9e3e442341920cfaaef90
Sadia Afrin Anika <anikasadia2723@gmail.com>
2026-01-31 17:35:46 +0600

    Implemented Two Sum(O(N))

---
+++ Month_01/Week_02/Day_14/Challenge_01.py
@@ -0,0 +1,34 @@
+# Two Sum
+list_A = [3,6,2,9,2,40,1,34,26,74,6,7]
+list_B = [56,3,12,7,4,88,26,24,7,22,12]
+
+target = 50
+
+# Brute Force Approach
+for i in list_A:
+    for j in list_B:
+        if i+j == target:
+            print(f"{i} + {j} = {target}")
+
+
+# Updated and Efficient approach 
+def add_sum(list_A, list_B):
+    needed=0
+    for i in range(list_A):
+        needed = target - i
+    for j in range(list_B):
+        if needed in range(list_B):
+            return {needed, i}
+        else:
+            return 0
+
+
+result = map(add_sum, list_A, list_B)
+print(list(result))
+
+# Output
+
+"""
+26 + 24 = 50
+[{48, 2}, 0, 0, 0, 0, {11, 39}, 0, {17, 33}, 0, 0, 0]
+"""
